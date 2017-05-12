edcb5025e14b904260b37bb90c65bd7c3b39d251
Akin Oluyele <waleander@gmail.com>
2017-05-12 09:44:25 +0100

    revert back to failing parameters

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var request = require("supertest");
 
 describe('GET /', function(){
   it('expects HTTP response code 200', function(done){
-    request(sails.hooks.http.index)
+    request(sails.hooks.http.app)
     .get('/')
     .expect(200, done);
   });
