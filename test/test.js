1a429f6be285d41a07423afe1a095b0d420a0353
Akin Oluyele <waleander@gmail.com>
2017-05-12 09:33:40 +0100

    change parameters to request in test.js

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var request = require("supertest");
 
 describe('GET /', function(){
   it('expects HTTP response code 200', function(done){
-    request(sails.hooks.http.app)
+    request(sails.hooks.http.index)
     .get('/')
     .expect(200, done);
   });
