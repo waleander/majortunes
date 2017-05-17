c3b282fe5354f1c697504f3cd432329a1ea65d53
Akin Oluyele <waleander@gmail.com>
2017-05-17 11:02:28 +0100

    move sails files into backend

---
+++ backend/test/test.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var sails = require('sails');
+var request = require("supertest");
+
+describe('GET /', function(){
+  it('expects HTTP response code 200', function(done){
+    request(sails.hooks.http.app)
+    .get('/')
+    .expect(200, done);
+  });
+});
